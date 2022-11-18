1dd6cd3d18a455191e06b217ecf50652de179c55
nikhil7299 <theavengernikhil@gmail.com>
2022-11-19 01:34:12 +0530

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,34 +30,59 @@ class _MyAppState extends State<MyApp> {
 }
 
 class Contact {
+  final String id;
   final String name;
-  const Contact({
+  Contact({
     required this.name,
-  });
+  }) : id = const Uuid().v4();
 }
 
 //Singleton class
-class ContactBook {
+class ContactBook extends ValueNotifier<List<Contact>> {
+  //ValueNotifier needs the value to be passed in constructor
+  //Now valuenotifier manages a value of type written after ValueNotifier
   //private constructor
-  ContactBook._sharedInstance();
+  ContactBook._sharedInstance() : super([]);
   //private object
   static final ContactBook _shared = ContactBook._sharedInstance();
   //factory keyword to implement constructors that do not produce
   //new instances of an existing class.
+
   factory ContactBook() => _shared;
-  final List<Contact> _contacts = [];
-  int get length => _contacts.length;
+
+  // final List<Contact> _contacts = []; No need as ValueNotifier has a value
+
+  // int get length => _contacts.length;
+  int get length => value.length;
 
   void add({required Contact contact}) {
-    _contacts.add(contact);
+    // _contacts.add(contact);
+    // As we changing the internals of value not the value itself
+    // so no notifyListener() is called, we can explicity call it or do this:
+    final contacts = value;
+    contacts.add(contact);
+    // value = contacts; no need as both are same
+    notifyListeners();
+
+    //OR
+    // value.add(contact);
+    // notifyListeners();
   }
 
   void remove({required Contact contact}) {
-    _contacts.remove(contact);
+    // _contacts.remove(contact);
+    // value.remove(contact);
+    final contacts = value;
+    if (contacts.contains(contact)) {
+      contacts.remove(contact);
+      // value = contacts;
+      notifyListeners();
+    }
   }
 
   Contact? contact({required int atIndex}) =>
-      _contacts.length > atIndex ? _contacts[atIndex] : null;
+      // _contacts.length > atIndex ? _contacts[atIndex] : null;
+      value.length > atIndex ? value[atIndex] : null;
 }
 
 class HomePage extends StatelessWidget {
@@ -64,20 +90,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final contactBook = ContactBook();
+    // final contactBook = ContactBook();
     return Scaffold(
       appBar: AppBar(
         title: Text("Contact Book"),
       ),
-      body: ListView.builder(
-        itemCount: contactBook.length,
-        itemBuilder: (context, index) {
-          final contact = contactBook.contact(atIndex: index)!;
-          return ListTile(
-            title: Text(contact.name),
-          );
-        },
-      ),
+      body: ValueListenableBuilder(
+          valueListenable: ContactBook(),
+          builder: (context, value, child) {
+            final contacts = value as List<Contact>;
+            return ListView.builder(
+              // itemCount: contactBook.length,
+              itemCount: contacts.length,
+              itemBuilder: (context, index) {
+                // final contact = contactBook.contact(atIndex: index)!;
+                final contact = contacts[index];
+                return Dismissible(
+                  onDismissed: (direction) {
+                    // contacts.remove(contact);
+                    ContactBook().remove(contact: contact);
+                  },
+                  key: ValueKey(contact.id),
+                  child: Material(
+                    color: Colors.white,
+                    elevation: 6.0,
+                    child: ListTile(
+                      title: Text(contact.name),
+                    ),
+                  ),
+                );
+              },
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await Navigator.of(context).pushNamed('/new-contact');
